2d058b4db55b6964dc0d28ed25fea25500f61d59
codeurient <codeurient@gmail.com>
2026-03-06 16:52:12 +0400

    analog input

--- test.c
+++ test.c
@@ -1,29 +1,20 @@
 #define LED_RED 6
-#define LED_BLUE 5
-
 #define BUTTON1 8
-#define BUTTON2 9
 
-int state = 0;
+bool state = false;
 
 void setup() {
     pinMode(LED_RED, OUTPUT);
-    pinMode(LED_BLUE, OUTPUT);
-
     pinMode(BUTTON1, INPUT);
-    pinMode(BUTTON2, INPUT);
-
-    Serial.begin(9600);
 }
 
 void loop() {
-    if(digitalRead(BUTTON1) == HIGH) {
-        state = !(state);
-        digitalWrite(LED_RED, HIGH);
-        digitalWrite(LED_BLUE, LOW);
-    } else if(digitalRead(BUTTON2) == HIGH) {
-        state = !(state);
-        digitalWrite(LED_RED, LOW);
-        digitalWrite(LED_BLUE, HIGH);
+    if (digitalRead(BUTTON1) == HIGH) {
+        state = !state;                 // vəziyyəti dəyiş (on/off)
+        digitalWrite(LED_RED, state);   // LED-i yeni vəziyyətə görə yandır/söndür
+
+        while (digitalRead(BUTTON1) == HIGH) {
+            delay(1);                   // düymə buraxılana qədər gözlə
+        }
     }
 }
